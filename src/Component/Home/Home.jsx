5e488a0aab17e13509df3de0d757d98ed685bc4b
imranahmed20 <108649863+imranahmed20@users.noreply.github.com>
2023-05-02 01:14:50 +0600

    cart detail added

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Banner from '../Page/Banner/Banner';
+import CardDetail from '../Page/CardDetail/CardDetail';
+
 
 const Home = () => {
     return (
         <div className='container'>
             <Banner></Banner>
-            <h1>THis is home</h1>
+            <CardDetail></CardDetail>
         </div>
     );
 };
