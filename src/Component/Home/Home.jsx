fdb3ce7fc51fa69085c860dbecd1b409d738e5b1
imranahmed20 <108649863+imranahmed20@users.noreply.github.com>
2023-05-02 01:48:46 +0600

    card section added and data load

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import Banner from '../Page/Banner/Banner';
 import CardDetail from '../Page/CardDetail/CardDetail';
+import { useLoaderData } from 'react-router-dom';
+import Category from '../Page/Category/Category';
 
 
 const Home = () => {
+    const categories = useLoaderData()
+    console.log(categories)
     return (
         <div className='container'>
             <Banner></Banner>
             <CardDetail></CardDetail>
+            <div className="row row-cols-1 row-cols-md-3 g-4">
+                {
+                    categories.map(chef => <Category key={chef.id} chef={chef}></Category>)
+                }
+            </div>
         </div>
     );
 };
--- src/Component/Page/CardDetail/CardDetail.jsx
+++ src/Component/Page/CardDetail/CardDetail.jsx
@@ -5,19 +5,6 @@ const CardDetail = () => {
         <div className='mt-5'>
             <h1 className='text-center fw-bold'>Top Six Chefs In Bangladesh</h1>
             <p className='banner-detail text-center'>Chef responsibilities include studying recipes, setting up menus and preparing high-quality dishes. You should be able to delegate tasks to kitchen staff to ensure meals are prepared in a timely manner. Also, you should be familiar with sanitation regulations.</p>
-            <div>
-                <div className="row row-cols-1 row-cols-md-3 g-4">
-                    <div className="col">
-                        <div className="card h-100">
-                            <img src="..." class="card-img-top" alt="..." />
-                            <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
         </div>
 
 
