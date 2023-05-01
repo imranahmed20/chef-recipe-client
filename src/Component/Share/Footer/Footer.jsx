bf740f23d23107aac36994b5a92bc53d263e7b72
imranahmed20 <108649863+imranahmed20@users.noreply.github.com>
2023-05-01 23:47:56 +0600

    navbar added

---
+++ src/Component/Share/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <p><small>Copy right is here</small></p>
+        </div>
+    );
+};
+
+export default Footer;
--- src/main.jsx
+++ src/main.jsx
@@ -7,10 +7,11 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Main from './Component/Layout/Main.jsx';
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Main></Main>,
   },
 ]);
 
