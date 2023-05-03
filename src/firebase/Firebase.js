9e5f7378f2307bdbcb40db7dafc93c5708ff2898
imranahmed20 <108649863+imranahmed20@users.noreply.github.com>
2023-05-03 13:58:58 +0600

    hide firebase config

--- src/firebase/Firebase.js
+++ src/firebase/Firebase.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCThknP8dl4d0BqGkjoV1iGL4t4i-UdzHU",
-    authDomain: "chef-recipe-client-15e57.firebaseapp.com",
-    projectId: "chef-recipe-client-15e57",
-    storageBucket: "chef-recipe-client-15e57.appspot.com",
-    messagingSenderId: "373634232164",
-    appId: "1:373634232164:web:fbf0c0eb6319a7f9cdd471"
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
